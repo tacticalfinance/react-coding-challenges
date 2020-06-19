648385f4135f2651f76036be4f1966c245282f3f
Ruby Salton <ruby@tornadofx.co.il>
2020-06-19 17:49:38 +0300

    rocket solution

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/RocketCore.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/RocketCore.js
@@ -6,12 +6,13 @@ const MS_TO_TAKEOFF = SECONDS_TO_TAKEOFF * 1000;
 const FINAL_POSITION_BOTTOM_VAL = 'calc(400px)';
 
 function timeToPositionPercent(startTime) {
-  const now = Date.now();
-  const timeDiff = now - startTime;
+  return FINAL_POSITION_BOTTOM_VAL;
+  // const now = Date.now();
+  // const timeDiff = now - startTime;
 
-  if (timeDiff >= MS_TO_TAKEOFF) { return FINAL_POSITION_BOTTOM_VAL; }
+  // if (timeDiff >= MS_TO_TAKEOFF) { return FINAL_POSITION_BOTTOM_VAL; }
 
-  return `calc(300px + ${((timeDiff / MS_TO_TAKEOFF) * 100).toFixed(0)}%)`;
+  // return `calc(300px + ${((timeDiff / MS_TO_TAKEOFF) * 100).toFixed(0)}%)`;
 }
 
 function generateEmptyListEls(quantity) {
